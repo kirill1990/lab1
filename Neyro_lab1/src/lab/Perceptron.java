5266cf65151e82e9a665f4e62dd63e18ea03816e
kirill1990 <demyanov.kirill@gmail.com>
2013-04-09 23:25:28 +0400

    update

--- Neyro_lab1/src/lab/Perceptron.java
+++ Neyro_lab1/src/lab/Perceptron.java
@@ -2,49 +2,43 @@ package lab;
 
 import java.util.Vector;
 
-public class Perceptron
-{
-	// количество входов сеть
-	// рисунок 16*16=256
-	private int				count_x			= 64*64;
+public class Perceptron {
+    // количество входов сеть
+    // рисунок 16*16=256
+    private int count_x = 64 * 64;
 
-	// количество нейронов
-	// количество цифр = 10
-	private int				count_neurons	= 10;
+    // количество нейронов
+    // количество цифр = 10
+    private int count_neurons = 10;
 
-	private Vector<Neuron>	neurons			= null;
+    private Vector<Neuron> neurons = null;
 
-	public Perceptron()
-	{
-		this.neurons = new Vector<Neuron>();
+    public Perceptron() {
+	this.neurons = new Vector<Neuron>();
 
-		for (int i = 0; i < this.count_neurons; i++)
-		{
-			this.neurons.add(new Neuron(this.count_x));
-		}
+	for (int i = 0; i < this.count_neurons; i++) {
+	    this.neurons.add(new Neuron(this.count_x));
+	}
+    }
+
+    /**
+     * Распознавание образа
+     * 
+     * @param x
+     *            - входной вектор
+     * @return выходной образ
+     */
+    public Vector<Double> recognize(Vector<Double> x) {
+	Vector<Double> y = new Vector<Double>();
+
+	for (int i = 0; i < neurons.size(); i++) {
+	    y.add(neurons.get(i).fActivation(x));
 	}
 
-	/**
-	 * Распознавание образа
-	 * 
-	 * @param x
-	 *            - входной вектор
-	 * @return выходной образ
-	 */
-	public Vector<Double> recognize(Vector<Double> x)
-	{
-		Vector<Double> y = new Vector<Double>();
+	return y;
+    }
 
-		for (int i = 0; i < neurons.size(); i++)
-		{
-			y.add(neurons.get(i).fActivation(x));
-		}
-
-		return y;
-	}
-	
-	public Vector<Neuron> getNeurons()
-	{
-		return neurons;
-	}
+    public Vector<Neuron> getNeurons() {
+	return neurons;
+    }
 }
